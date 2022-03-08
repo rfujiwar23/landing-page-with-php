5f290c73bb6e43013384be2308d2a9587c621dec
Ryo Fujiwara <jjcproject@icloud.com>
2022-03-08 16:09:20 +0900

    php site

---
+++ index.php
@@ -0,0 +1,136 @@
+<?php
+$title = "【公式】ASIAインピタリ トリートメント";
+$content = "ASIA INPITARI アジアインピタリ · アジアを代表する 史上最高の”2 STEP” トリートメント. ASIA インピタリ ケアシステム誕生。 · 美容業界の歴史を変える衝撃ブランドです！";
+$navbar = '
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="https://www.ifing.com/">
+      <img src="assets/images/ifing-logo-full.png" alt="ASIA" height="30" class="logo">
+    </a>
+  </div>
+</nav>
+';
+
+$footer = '
+  <section class="footer">
+    <div class="container">
+
+    <ul>
+      <li><a href="https://ifing-beauty.com/help/tradelaw">特定商取引法に基づく表記</a></li>
+      <li><a href="https://ifing-beauty.com/help/privacy">プライバシーポリシー</a></li>
+      <li><a href="https://ifing-beauty.com/help/about">当サイトについて</a></li>
+      <li><a href="https://www.ifing.com/company/">会社情報</a></li>
+    </ul>
+    <p>Copyright © IFing Co. Ltd. 2020 All Rights Reserved</p>
+    </div>
+
+  </section>
+';
+
+$section01 = '
+<section class="section01">
+    <div class="container">
+      <h2 class="mt-5 text-center top ">アジアを代表する<br>
+        史上最高の”2 STEP” トリートメント<br>
+        ASIA インピタリ ケアシステム誕生。</h2>
+        <br>
+        <h2 class="text-center top intro mt-5"><span>美容業界の歴史を<br>変える衝撃ブランドです！</span></h2>
+        <br>
+        <hr>
+        <br>
+      <h2 class="text-center top intro mt-2">これまでの<br class="br_on_12">2STEPトリートメントの課題</h2>
+      
+        
+        <div class="col-lg-8 offset-lg-2 my-5">
+          <table>
+            <tr>
+              <td class="title"><p>お客様：</p></td>
+              <td><p class="t-a">手軽に出来るが、ケア効果があまり感じられない</p></td>
+            </tr>
+            <tr>
+              <td class="title"><p>美容室：</p>
+              </td>
+              <td><p class="t-a">提案しやすいが、リピートしてもらえない</p></td>
+            </tr>
+          </table>
+        </div>
+
+      <h5 class="text-center mb-5">皆さん、こんなイメージはありませんか？</h5>
+
+      <hr>
+
+      <div class="voices">
+          <h4 class="px-2">Salon User Voice</h4>
+        <div class="row col-lg-10 offset-lg-1 col-md-10 offset-md-1 col-sm-12 stylists reverse">
+          <div class="col-lg-8 col-md-7 col-sm-8 my-auto">
+            <div class="v-text">
+              <h5>cache cache 東京・表参道</h5>
+              <p>前田渉氏</p>
+                <blockquote><strong>ASIA インピタリは、とにかく『ツヤ』『手触り』が実感できるシンプルなメニュー<br>
+                  それでいて、確実に『補修』と『保湿』ができる点が魅力</strong>
+                    <br>
+                    お客様がデザインカラー等で時間を伴う施術の中で、短時間で結果が出せるメニューとしてASIA インピタリは非常に使いやすいですね。<br>
+                    特にヘアカラーの褪色が気にならず、デザインの邪魔をぜずにケアできるのがポイントです。
+                </blockquote>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-5 col-sm-4 col-8 p-0 profile-pic">
+            <img src="assets/images/img-stylist001.png" alt="cache cache 前田氏" class="img-fluid">
+          </div>
+        </div>
+        <div class="row col-lg-10 offset-lg-1 col-md-10 offset-md-1 col-sm-12 stylists">
+          <div class="col-lg-4 col-md-5 col-sm-4 col-8 p-0 profile-pic">
+            <img src="assets/images/img-stylist003.png" alt="cache cache 前田氏" class="img-fluid">
+          </div>
+          <div class="col-lg-8 col-md-7 col-sm-8 my-auto">
+            <div class="v-text">
+              <h5>iro. 東京・代官山</h5>
+              <p>樗木佑太氏</p>
+              <blockquote><strong>ASIAは、説得力があるトリートメント</strong><br>
+                <strong>スピーディーなのに嘘がない</strong><br>
+                「お客様の求める色」にする為には、ASIA インピタリは欠かせません。たった1回の施術で
+                「お客様の求める色」にするのは難しい場合が多いです。 iro.では、それを実現させる為に
+                「長期的なプラン」を提案しているのですが、その中心になるのはASIA インピタリです。</blockquote>
+            </div>
+          </div>
+        </div>
+        <div class="row col-lg-10 offset-lg-1 col-md-10 offset-md-1 col-sm-12 stylists reverse">
+          <div class="col-lg-8 col-md-7 col-sm-8 my-auto">
+            <div class="v-text">
+              <h5>Laji/RUNO 大阪・北浜/東京・青山</h5>
+              <p>EIKO氏</p>
+                <blockquote><strong>５分でカラー褪色しない強い艶髪に</strong><br>
+                  <strong>サロンメニューとして当日予約出来る本質ケアはASIA インピタリだけ</strong><br>
+                  カラー・パーマにいかに艶を出して持続させるかという点でASIA インピタリは
+                  すごく良いです。 軽くて＋艶。軽いけど収まる。ASIA インピタリ をして
+                  再来店の時のコンディションがすごくいい。 
+                  続ける事で更にカラー褪色がしなくなると思います。</blockquote>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-5 col-sm-4 col-8 p-0 profile-pic">
+            <img src="assets/images/img-stylist002.png" alt="cache cache 前田氏" class="img-fluid">
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 text-center intro-text">
+        <hr>
+        <h6><span>インピタリ</span>はそんな常識を覆します！</h6>
+        <h4>特許レベルの化学反応<span><br class="br_on_12">「インピタリ」</span>と<br>
+          2021年最高レベルの<br class="br_on_1024">ケア成分<br class="br_on_12">（補修成分8種類・保湿成分11種類）により<br>
+          4STEP級の効果を手に入れることができます！</h4>
+      </div>
+
+      
+
+    </div>
+    <div>
+      <h6 class="my-3 text-center intro-text-out">さらに<span>サロンケア</span>と<span>ホームケア</span><br class="br_on_576">の連動によって、
+        <br class="br_on_1024">新しいケアループを<br class="br_on_12">ご提案します。</h6>  
+    </div>
+  </section>
+';
+
+
+include 'template.php';
+?>
---
+++ template.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap 5.0 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <!-- Link to CSS -->
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <title><?php echo $title; ?></title>
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/favicon/site.webmanifest">
+</head>
+<body>
+    
+
+
+<?php echo $navbar; ?>
+
+<?php echo file_get_contents("banner.html"); ?>
+
+
+
+
+
+<div id="main">
+
+
+  <?php echo $section01; ?>
+
+
+  <section class="sect-middle">
+    <div class="container">
+     <h3 class="text-center">お伝えしたいポイントは３つ！</h3>
+    </div>
+  </section>
+
+
+  <?php echo file_get_contents("section02.html"); ?>
+
+  <?php echo file_get_contents("section03.html"); ?>
+
+  <?php echo file_get_contents("section04.html"); ?>
+
+
+
+  <section class="section05">
+   <div class="container">
+    <div class="sect06-body">
+      <h2 class="text-center my-5">全てを満たすのは<br class="br_on_12">ASIAインピタリだけ!!<br>
+        2STEPトリートメントで<br class="br_on_12">ここまでできる！<br>
+        最強の「インピタリ」を<br class="br_on_12">ぜひ体験してください。</h2>
+    </div>
+   </div>
+  </section>
+
+  <section class="section06">
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-6 col-md-6 col-sm-6 to-links">
+         <p class="text-center salon"><a href="https://www.ifing.com/asia/">美容師・美容室の方はこちら</a></p>
+       </div>
+       <div class="col-lg-6 col-md-6 col-sm-6 to-links">
+         <p class="text-center customer"><a href="https://www.ifing.com/asia/line-up/asia-inpitari-home-care/">一般のお客様はこちら</a></p>
+      </div>
+     </div>
+   </div>
+  </section>
+
+  <?php echo $footer; ?>
+  
+</div>
+
+
+    
+</body>
+</html>
